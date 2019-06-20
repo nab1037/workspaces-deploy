8c3a499590b41629d3785654a7669858601fdb37
nab1037 <41760691+nab1037@users.noreply.github.com>
2019-06-20 19:11:31 -0400

    Update config.js

--- website/js/config.js
+++ website/js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_ABCDEFG', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '1a2b3c4d5f', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'us-east-1' // e.g. us-east-2
+        userPoolId: 'us-west-2_2bjxh4Ate', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '5o06s93m83umi5l6blra6dfj3c', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-west-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: 'https://2nkt5nrnm8.execute-api.us-west-2.amazonaws.com/Prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
